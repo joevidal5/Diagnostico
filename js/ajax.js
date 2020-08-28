43c3300ff02b055aae5512a666c0cbad204015c5
joevidal5 <62678280+joevidal5@users.noreply.github.com>
2020-08-27 21:59:16 -0300

    diagnostico

--- js/ajax.js
+++ js/ajax.js
@@ -4,6 +4,66 @@ $(document).ready(function(){
         $('#principal').load("productos/anillos.html");
     })
 
+    $('#collares').click(function(){
+        $('#principal').load("productos/collares.html");
+    })
+
+    $('#comprar').click(function(){
+        $('#principal').load("buy.html");
+    })
+
+    $('#comprar1').click(function(){
+        $('#principal').load("buy.html");
+    })
+
+    $('#comprar2').click(function(){
+        $('#principal').load("buy.html");
+    })
+    
+    $('#comprar3').click(function(){
+        $('#principal').load("buy.html");
+    })
+    
+    $('#comprar3').click(function(){
+        $('#principal').load("buy.html");
+    })
+    
+    $('#comprar4').click(function(){
+        $('#principal').load("buy.html");
+    })
+    
+    $('#comprar5').click(function(){
+        $('#principal').load("buy.html");
+    })
+    
+    $('#comprar6').click(function(){
+        $('#principal').load("buy.html");
+    })
+
+    $('#comprar7').click(function(){
+        $('#principal').load("buy.html");
+    })
+
+    $('#outlet').click(function(){
+        $('#principal').load("outlet/outlet.html");
+    })
+
+    $('#outletPropaganda').click(function(){
+        $('#principal').load("outlet/outlet.html");
+    })
+
+    $('#comprarFinalizada').click(function(){
+        $('#principal').load('compraFinalizada.html');
+    })
+
+    $('#consulta').click(function(){
+        $('#principal').load('consulta.html');
+    })
+
+    $('#enviar').click(function(){
+        $('#principal').load('consultaFinalizada.html');
+    })
+
     $("#logo").click(function(){
         location.reload(true);
     })
