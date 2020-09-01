1e62af300bcd071a2ba2a3c53292b2a99c6d16b9
joevidal5 <62678280+joevidal5@users.noreply.github.com>
2020-09-01 11:01:04 -0300

    flask

--- js/buyajax.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('#comprar').click(function(){
-        $('#principal').load('successful_purchase.html')
-    })
-});
---
+++ static/js/ajax.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+
+    $('#anillos').click(function(){
+        $('#principal').load("../static/productos/anillos.html");
+    })
+
+    $('#collares').click(function(){
+        $('#principal').load("../static/productos/collares.html");
+    })
+
+    $('#comprar').click(function(){
+        $('#principal').load("../../templates/buy.html");
+    })
+
+    $('#comprar1').click(function(){
+        $('#principal').load("../../templates/buy.html");
+    })
+
+    $('#comprar2').click(function(){
+        $('#principal').load("../../templates/buy.html");
+    })
+    
+    $('#comprar3').click(function(){
+        $('#principal').load("../../templates/buy.html");
+    })
+    
+    $('#comprar3').click(function(){
+        $('#principal').load("../../templates/buy.html");
+    })
+    
+    $('#comprar4').click(function(){
+        $('#principal').load("../../templates/buy.html");
+    })
+    
+    $('#comprar5').click(function(){
+        $('#principal').load("../../templates/buy.html");
+    })
+    
+    $('#comprar6').click(function(){
+        $('#principal').load("../../templates/buy.html");
+    })
+
+    $('#comprar7').click(function(){
+        $('#principal').load("../../templates/buy.html");
+    })
+
+    $('#outlet').click(function(){
+        $('#principal').load("../static/outlet/outlet.html");
+    })
+
+    $('#outletPropaganda').click(function(){
+        $('#principal').load("../static/outlet/outlet.html");
+    })
+
+    $('#comprarFinalizada').click(function(){
+        $('#principal').load('../../templates/compraFinalizada.html');
+    })
+
+    $('#consulta').click(function(){
+        $('#principal').load('../../templates/consulta.html');
+    })
+
+    $('#enviar').click(function(){
+        $('#principal').load('../templates/consultaFinalizada.html');
+    })
+
+    $("#logo").click(function(){
+        location.reload(true);
+    })
+
+    $("#home").click(function(){
+        location.reload(true);
+    })
+
+   
+});
